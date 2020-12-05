0686c4babe8d103805c98603e3a8c3b7088257db
emanherawy <eman.herawy@gmail.com>
2020-12-06 01:01:09 +0200

    deploy to ropsten network

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,42 @@
+const UniswapV2Router02 = artifacts.require("UniswapV2Router02");
+const WETH9 = artifacts.require("WETH9");
+module.exports = async function (deployer, network, accounts) {
+  // Use deployer to state migration tasks.
+   // set address based on network 
+  const {
+    factory,
+    weth
+  } = await getAddress(network,deployer);
+console.log(factory,
+  weth,'factory,weth');
+
+
+  await deployer.deploy(UniswapV2Router02, factory, weth);
+
+};
+
+async function getAddress(network, deployer) {
+  if (network === 'development') {
+    await deployer.deploy(WETH9);
+    const weth =WETH9.address;
+    const factory = "0x3488b1A29157A49F4029C28acB1625Cf8a2Cd73F";
+       return ({factory, weth})
+  } else if (network === 'ropsten' || network === 'ropsten-fork') {
+    //https://ropsten.etherscan.io/token/0xb603cea165119701b58d56d10d2060fbfb3efad8
+    const weth = "0xb603cea165119701b58d56d10d2060fbfb3efad8";
+    const factory = "0xa90CE65203cB9Bf7e2c1c47549C2bf6296CECB32";
+    return ({factory, weth})
+  } else if (network === 'kovan') {
+    const weth  ="" //TODO;
+    const factory  ="" //TODO;
+    return ({factory, weth})
+  } else if (network === 'main') {
+      // https://etherscan.io/token/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
+
+    const weth = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+    const factory ="" //TODO;
+    return ({factory, weth})
+  } else {
+    return (null,null)
+  }
+}
--- truffle-config.js
+++ truffle-config.js
@@ -28,7 +28,7 @@ module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
+   * will spin up a development blockchain for you on 0.6.6.6.6rt 9545 when you
    * run `develop` or `test`. You can ask a truffle command to use a specific
    * network from the command line, e.g
    *
@@ -45,6 +45,9 @@ module.exports = {
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
+     gas: 3903258,           // Gas sent with each transaction (default: ~6700000)
+      gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    
      network_id: "*",       // Any network (default: none)
     },
 
@@ -85,15 +88,15 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      version: "0.6.6",    // Fetch exact version from solc-bin (default: truffle's version)
+      //docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: true,
+         runs: 200
+       },
+       evmVersion: "istanbul"
+      }
     }
   }
 }
