2af1b02fd89b8b5624dd2f311bef7cab6197a1ef
emanherawy <eman.herawy@gmail.com>
2021-01-21 17:24:48 +0200

    update init code hash

--- contracts/libraries/SeaSwapV2Library.sol
+++ contracts/libraries/SeaSwapV2Library.sol
@@ -21,7 +21,7 @@ library SeaSwapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'275e88a10e28e43281ab96948fe2079633db9e4eef0d4f6c9a2c2ece1cc8a4ca' // init code hash for seaswap
+                hex'c2202e4ff587396e82a39b15c07b6ad13f1e8a6b4b8331c90eaf9979a6af1f3e' // init code hash for seaswap
                 // hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
             ))));
     }
